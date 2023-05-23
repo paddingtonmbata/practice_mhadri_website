75a5d013cef4c2a6097fe2726a8da33c2fef13dc
Kuda Mbata <kudambata2005@gmail.com>
2023-05-23 13:19:01 +0200

    latest_changes

--- dataBase/models.py
+++ dataBase/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class CountryData(models.Model):
+    country_name = models.CharField(max_length=100)
+    data_value = models.IntegerField()
+
+    def __str__(self):
+        return self.country_name
